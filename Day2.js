1b2fff16d219939e280d768d71072be5687124f3
Barada Prasanna Pradhan <baradaprasanna03@protonmail.com>
2023-03-15 00:49:58 +0530

    Initial commit

---
+++ Day2.js
@@ -0,0 +1,17 @@
+
+document.write("this is document.write <br>");
+
+// this is a function
+function msg(){
+    alert("hello");
+}
+
+window.alert("this is a windows alert");
+document.write("\n this is the write.document");
+console.log("hello\ngyus '\\n' is use for new line in console");
+document.getElementById("dom").innerHTML="hello this DOM";
+function myFunction() {
+    document.getElementById("demo").innerHTML = "I have changed!";
+  }
+
+
