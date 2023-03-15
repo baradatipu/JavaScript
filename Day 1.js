0cfe0353d542365065176338b1f95ea6d7b1dcb9
Barada Prasanna Pradhan <baradaprasanna03@protonmail.com>
2023-03-15 17:21:48 +0530

    update

---
+++ Day 1.js
@@ -0,0 +1,2 @@
+console.log(" this is console.log.\n It print on only on console.\n This code is placed on ecternal js file.");
+document.write("this is document.write this code placed on external javascript file");
